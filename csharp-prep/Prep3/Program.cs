200c753995e42769d050581cc32735d73a98e283
jordan-ward <fallbrook43@gmail.com>
2024-11-05 19:14:50 -0700

    Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("The magic number is random!");
+
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You got it!");
+            }
+
+        } while (guess != number);
+
     }
 }
