dffb7b61bfc07ee79bf191d5a709a31c49bc4e11
jordan-ward <fallbrook43@gmail.com>
2024-11-22 22:06:16 -0700

    Update

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,31 @@
+using System.Runtime.CompilerServices;
+
+public class Word
+{
+    private string _text {get;}
+    private bool _isHidden{get; set;}
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public string GetDisplayText()
+    {
+        if
+        {
+            Hide
+        }
+    }
+}
